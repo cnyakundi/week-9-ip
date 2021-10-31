5f077ed866ce280ccaf448f52da1a5949094ad18
Cyprian Nyakundi <cypriannyakundi@gmail.com>
2021-10-31 19:45:15 +0300

    create scripts

--- scripts/spambase.py
+++ scripts/spambase.py
@@ -11,12 +11,13 @@ import matplotlib.pyplot as plt
 
 import seaborn as sns 
 
-# import warnings
-# warnings.filterwarnings('ignore')
+import warnings
+warnings.filterwarnings('ignore')
 
 
 
 # Importing the dataset
+
 spambase= pd.read_csv('../spambase.csv')
 X = spambase.drop("1",axis=1).values
 y = spambase["1"].values
@@ -27,9 +28,14 @@ y = spambase["1"].values
 
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state=25)
+
+print("===========================================================")
 print(X_train)
+print("===========================================================")
 print(y_train)
+print("============================================================")
 print(X_test)
+print("============================================================")
 print(y_test)
 
 
@@ -41,7 +47,9 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
+print("=======================================================")
 print(X_train)
+print("=======================================================")
 print(X_test)
 
 
@@ -50,11 +58,13 @@ print(X_test)
 
 # Previewing the shapes of X-Train and Y-Train 
 
-
+print("===========================================================")
 print(X_train.shape)
+print("===========================================================")
 print(y_train.shape)
-
+print("===========================================================")
 print(X_test.shape)
+print("===========================================================")
 print(y_test.shape)
 
 
@@ -73,8 +83,9 @@ classifier.fit(X_train, y_train)
 
 
 y_pred = classifier.predict(X_test)
+print("===========================================================")
 print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
-
+print("===========================================================")
 
 
 # Making the Confusion Matrix
@@ -83,9 +94,13 @@ print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 cm = confusion_matrix(y_test, y_pred)
 classification_report =classification_report(y_test, y_pred)
+print("===========================================================")
 print(cm)
+print("===========================================================")
 print(classification_report)
+print("===========================================================")
 accuracy_score(y_test, y_pred)
-print(f"Our accuracy score is {accuracy_score} ")
+print(f"Our accuracy score is {accuracy_score(y_test, y_pred)} ")
+print("===========================================================")
 
 
